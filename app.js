d19490c1111a27477ed01c32a1075659f0b3ee1a
Alma Ramona Bolaños Villegas <gorduela04@hotmail.com>
2022-09-22 12:01:03 -0500

    bonus

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-// app.get('/', function(req, res) {
-//     res.send('Hello world');
-// });
+app.get('/', function(req, res) {
+    res.send('Hello world');
+});
 
 // app.use('/api', require('./routers/router'));
 app.use('/', require('./routers/router'));
